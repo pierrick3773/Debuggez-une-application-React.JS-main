2f0fe57d34f02dda674d75f21385d8bd093a8126
pierrick3773 <pviarde@gmail.com>
2024-04-09 10:00:14 +0200

    tableau correcte helpers / date

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -6,6 +6,7 @@ import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
+  console.log(data);
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
