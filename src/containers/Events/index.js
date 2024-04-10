66f16f79bf25eefc20721d55411e3b7a91b0579b
pierrick3773 <pviarde@gmail.com>
2024-04-10 18:07:04 +0200

    correction pour le filtre par type d'event

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,17 +13,19 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = ((!type ? data?.events : data?.events) || []).filter(
-    (event, index) => {
-      if (
-        (currentPage - 1) * PER_PAGE <= index &&
-        PER_PAGE * currentPage > index
-      ) {
-        return true;
-      }
-      return false;
+  const filteredEvents = (
+    (!type
+      ? data?.events
+      : data?.events.filter((event) => event.type === type)) || []
+  ).filter((event) => {
+    if (
+      (currentPage - 1) * PER_PAGE <= event.id &&
+      PER_PAGE * currentPage > event.id
+    ) {
+      return true;
     }
-  );
+    return false;
+  });
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
